1bb814da2a2cd59d49652e763caee851388a08e5
“Leandro <leandrogregory1@gmail.com>
2025-04-25 23:26:35 -0300

    Commit 02: Autenticação do usuario e senha na classe conectaDAO

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,13 +4,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author Adm
@@ -22,7 +15,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","leomysql01");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
